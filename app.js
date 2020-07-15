2c22ad4bc2851844bff382864c570d6183bb3adc
davidalagoa <davidalagoa@gmail.com>
2020-07-15 10:20:07 +0100

    Update

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const productsDOM = document.querySelector(".product-center");
 // cart
 let cart = [];
 
+// buttons
+let buttonsDOM = [];
+
 // getting the products
 class Products {
     async getProducts() {
@@ -57,6 +60,7 @@ class UI {
     }
     getBagButtons() {
         const buttons = [...document.querySelectorAll('.bag-btn')];
+        buttonsDOM = buttons;
         buttons.forEach(button => {
             let id = button.dataset.id;
             let inCart = cart.find(item => item.id === id);
@@ -68,20 +72,68 @@ class UI {
                 event.target.innerText = "In Cart";
                 event.target.disabled = true;
                 // get product from products
+                let cartItem = {...Storage.getProduct(id), amount: 1 };
+
                 // add product to the cart
+                cart = [...cart, cartItem];
+
                 // save cart in local storage
+                Storage.saveCart(cart)
+
                 // set cart values
+                this.setCartValues(cart);
+
                 // display cart item
-                // show the cart
+                this.addCartItem(cartItem)
+                    // show the cart
             });
-        })
+        });
     }
-}
+    setCartValues(cart) {
+        let tempTotal = 0;
+        let itemsTotal = 0;
+        cart.map(item => {
+            tempTotal += item.price * item.amount;
+            itemsTotal += item.amount;
+        })
+        cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+        cartItems.innerText = itemsTotal;
+    };
+
+    addCartItem(item) {
+        const div = document.createElement('div');
+        div.classList.add('cart-item');
+        div.innerHTML = `
+        <img src=${item.image} alt="product">
+                    <div>
+                        <h4>${item.title}</h4>
+                        <h5>$${item.price}</h5>
+                        <span class="remove-item" data-id=${item.id}>remove</span>
+                    </div>
+                    <div>
+                        <i class="fas fa-chevron-up" data-id=${item.id}></i>
+                        <p class="item-amount">${item.amount}</p>
+                        <i class="fas fa-chevron-down" data-id=${item.id}></i>
+                    </div>`;
+        cartContent.appendChild(div);
+        console.log(cartContent);
+    }
+
+};
 
 // local storage
 class Storage {
     static saveProducts(products) {
         localStorage.setItem("products", JSON.stringify(products));
+    }
+
+    static getProduct(id) {
+        let products = JSON.parse(localStorage.getItem('products'));
+        return products.find(product => product.id === id);
+    }
+
+    static saveCart(cart) {
+        localStorage.setItem('cart', JSON.stringify(cart));
     }
 }
 
@@ -93,8 +145,8 @@ document.addEventListener("DOMContentLoaded", () => {
     products.getProducts().then(products => {
         ui.displayProducts(products);
         Storage.saveProducts(products);
-    }).then() => {
+    }).then(() => {
         ui.getBagButtons();
-    };
+    });
 
 });
