30bf68ad4b304b8fc349d77d3061e376b31a85fa
davidalagoa <davidalagoa@gmail.com>
2020-07-21 09:51:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ class UI {
                     <img src=${product.image} alt="product" class="product-img">
                     <button class="bag-btn" data-id=${product.id}>
                     <i class="fas fa-shopping-cart"></i>
-                    add to bag
+                    add to cart
                 </button>
                 </div>
                 <h3>${product.title}</h3>
@@ -142,6 +142,7 @@ class UI {
         cartDOM.classList.remove('showCart');
     }
 
+
     cartLogic() {
         // clear cart button
         clearCartBtn.addEventListener('click', () => {
@@ -149,11 +150,45 @@ class UI {
         });
 
         // cart functionality
+        cartContent.addEventListener('click', event => {
+            if (event.target.classList.contains('remove-item')) {
+                let removeItem = event.target;
+                let id = removeItem.dataset.id;
+                cartContent.removeChild(removeItem.parentElement.parentElement);
+                this.removeItem(id);
+            } else if (event.target.classList.contains('fa-chevron-up')) {
+                let addAmount = event.target;
+                let id = addAmount.dataset.id;
+                let tempItem = cart.find(item => item.id === id);
+                tempItem.amount = tempItem.amount + 1;
+                Storage.saveCart(cart);
+                this.setCartValues(cart);
+                addAmount.nextElementSibling.innerText = tempItem.amount;
+            } else if (event.target.classList.contains('fa-chevron-down')) {
+                let lowerAmount = event.target;
+                let id = lowerAmount.dataset.id;
+                let tempItem = cart.find(item => item.id === id);
+                tempItem.amount = tempItem.amount - 1;
+                if (tempItem.amount > 0) {
+                    Storage.saveCart(cart);
+                    this.setCartValues(cart);
+                    lowerAmount.previousElementSibling.innerText = tempItem.amount;
+                } else {
+                    cartContent.removeChild(lowerAmount.parentElement.parentElement);
+                    this.removeItem(id);
+                }
+
+            }
+        });
     }
 
     clearCart() {
         let cartItems = cart.map(item => item.id);
         cartItems.forEach(id => this.removeItem(id));
+        while (cartContent.children.length > 0) {
+            cartContent.removeChild(cartContent.children[0])
+        }
+        this.hideCart();
     }
 
     removeItem(id) {
@@ -162,7 +197,7 @@ class UI {
         Storage.saveCart(cart);
         let button = this.getSingleButton(id);
         button.disabled = false;
-        button.innerHTML = `<i c;ass="fas fa-shopping-cart></i>add to cart`;
+        button.innerHTML = `<i class="fas fa-shopping-cart"></i>add to cart`;
     }
 
     getSingleButton(id) {
